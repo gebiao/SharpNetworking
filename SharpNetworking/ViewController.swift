1283a482633ed44468c666fab143681ffb560889
gebiao <gebiao@360.cn>
2016-01-11 13:59:40 +0800

    no message

--- SharpNetworking/ViewController.swift
+++ SharpNetworking/ViewController.swift
@@ -11,8 +11,12 @@ import UIKit
 let videoUrl = "http://video.dispatch.tc.qq.com/54371892/f0016kk9vtn.p202.1.mp4?sdtfrom=v1001&type=mp4&vkey=90E374AF07B43A0DDC3AB1CD8029A3B268163270243D2861263581348E0E45EF16CEDF56740EEA453452DC95A59BCDA7A005F3A76ABF832CF082E1F03F1621A93DCEA04385442D4FAEBCE4C13303867396A435F36174BDDE&platform=11&br=98&fmt=hd&sp=350&guid=2AD956E7F3764B1351993F3D1F7ECC6FF7CAECFD"
 let imageUrl = "http://farm3.staticflickr.com/2846/9823925914_78cd653ac9_b_d.jpg"
 //"https://upload.wikimedia.org/wikipedia/commons/6/69/NASA-HS201427a-HubbleUltraDeepField2014-20140603.jpg"
+
+
 class ViewController: UIViewController {
     
+    @IBOutlet weak var backImageView: UIImageView!
+    @IBOutlet weak var progressLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -20,7 +24,14 @@ class ViewController: UIViewController {
         let request = download(.GET, URLString: imageUrl, destination: destination)
         
     }
+    @IBAction func startAction(sender: AnyObject) {
+    }
     
+    @IBAction func suspendAction(sender: AnyObject) {
+    }
+    
+    @IBAction func cancelAction(sender: AnyObject) {
+    }
     
 }
 
