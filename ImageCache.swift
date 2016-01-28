2e26aae1b39f832177d158cf68ae74fe603b4c41
gebiao <gebiao@360.cn>
2016-01-28 19:03:29 +0800

    no message

--- ImageCache.swift
+++ ImageCache.swift
@@ -79,7 +79,21 @@ extension UIImage {
     }
 }
 
-
+extension String {
+    func md5(string string: String) -> String {
+        var digest = [UInt8](count: Int(CC_MD5_DIGEST_LENGTH), repeatedValue: 0)
+        if let data = string.dataUsingEncoding(NSUTF8StringEncoding) {
+            CC_MD5(data.bytes, CC_LONG(data.length), &digest)
+        }
+        
+        var digestHex = ""
+        for index in 0..<Int(CC_MD5_DIGEST_LENGTH) {
+            digestHex += String(format: "%02x", digest[index])
+        }
+        
+        return digestHex
+    }
+}
 
 
 
---
+++ SharpNetworking/bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  SharpNetworking
+//
+//  Created by SharpSnake on 1/28/16.
+//  Copyright © 2016 GB Softwore Foundation. All rights reserved.
+//
+
+#ifndef bridge_h
+#define bridge_h
+
+#import <CommonCrypto/CommonCrypto.h>
+
+#endif /* bridge_h */
